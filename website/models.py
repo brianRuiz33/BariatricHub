1262f9e6e598a5cb46ed7c49753e6e7b76440c32
Brian Ruiz <braymr33@gmail.com>
2026-01-31 13:19:32 -0600

    Adding models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Reviews
+class Review(models.Model):
+    date_review = models.DateField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    stars = models.PositiveSmallIntegerField(default=5)
+
+# Contact
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=20)
+    reason = models.CharField(max_length=150)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+     
+#Patient
