3914f7b7004aee9a5a00e81f89dd512321510ced
Brian Ruiz <braymr33@gmail.com>
2026-01-30 18:58:48 -0600

    Fixes

--- website/static/website/js/main.js
+++ website/static/website/js/main.js
@@ -3,12 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
 // Image Swiper
 // JS
    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
       spaceBetween: 30,
+        loop: true,
+
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
+      breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
     });
 
 
